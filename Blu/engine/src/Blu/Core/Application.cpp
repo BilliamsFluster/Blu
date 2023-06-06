5015240ec212e43b254d610a31dc485ca89092a1
William <50932749+BilliamsFluster@users.noreply.github.com>
2023-06-05 21:25:27 -0400

    Improved Vertex buffer layouts

--- Blu/engine/src/Blu/Core/Application.cpp
+++ Blu/engine/src/Blu/Core/Application.cpp
@@ -16,6 +16,24 @@ namespace Blu
 	
 
 	Application* Application::s_Instance = nullptr;
+	static GLenum ShaderDataTypeToOpenGlBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:		return	GL_FLOAT;
+		case ShaderDataType::Float2:	return  GL_FLOAT;
+		case ShaderDataType::Float3:	return  GL_FLOAT;
+		case ShaderDataType::Float4:	return  GL_FLOAT;
+		case ShaderDataType::Mat3:		return  GL_FLOAT;
+		case ShaderDataType::Mat4:		return  GL_FLOAT;
+		case ShaderDataType::Int:		return	GL_INT;
+		case ShaderDataType::Int2:		return	GL_INT;
+		case ShaderDataType::Int3:		return	GL_INT;
+		case ShaderDataType::Int4:		return	GL_INT;
+		case ShaderDataType::Bool:		return	GL_BOOL;
+		}
+		return 0;
+	}
 	Application::Application()
 	{
 		m_Color = { 1,1,1,1 };
@@ -27,16 +45,34 @@ namespace Blu
 		glBindVertexArray(m_VertexArray);
 
 		
-		float vertices[3 * 3] = {
-			-0.8f, -0.3f, 0.0f,
-			-0.4f, -0.8f, 0.0f,
-			-0.1f, -0.3f, 0.0f
+		float vertices[3 * 7] = {
+			-0.8f, -0.3f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			-0.4f, -0.8f, 0.0f, 0.2f, 0.3f, 0.6f, 1.0f,
+			-0.1f, -0.3f, 0.0f, 0.3f, 0.8f, 0.2f, 1.0f
 		};
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
+		{
+			BufferLayout layout = {
+			{ShaderDataType::Float3, "a_Position"},
+			{ShaderDataType::Float4, "a_Color"}
+			//{ShaderDataType::Float3, "a_Normal", true}
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
+		
 
+		uint32_t index = 0;
+		for (const auto& element : m_VertexBuffer->GetLayout())
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(),
+				ShaderDataTypeToOpenGlBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE, m_VertexBuffer->GetLayout().GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		
 
 		
 
@@ -65,16 +101,23 @@ namespace Blu
 
 		// Unbind to reset to default framebuffer
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+		std::cout << "OpenGl Error: " << glGetError() << std::endl;
+		// no 1281 error here but error 0 here
+
 		std::string vertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
-				gl_Position = vec4(a_Position, 1.0);	
 				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);	
+				v_Color = a_Color;
 			}
 
 
@@ -87,19 +130,20 @@ namespace Blu
 			layout(location = 0) out vec4 o_color;
 			in vec3 v_Position;
 			uniform vec4 u_Color;
-
+			in vec4 v_Color;
 
 			void main()
 			{
-				o_color = u_Color;
+				o_color = v_Color;
 			}
 
 
 
 		)";
 		m_Shader.reset(new OpenGLShader(vertexSrc, fragmentSrc));
+		std::cout << "OpenGl Error: " << glGetError() << std::endl; // Error 0
 	}
-
+	
 	Application::~Application()
 	{
 
@@ -127,12 +171,15 @@ namespace Blu
 
 			m_Window->OnUpdate();
 			m_Running = !m_Window->ShouldClose();
+			std::cout << "OpenGl Error: " << glGetError() << std::endl; // Error 1281
+
 			m_Shader->Bind();
+
 			glBindFramebuffer(GL_FRAMEBUFFER, m_FrameBufferObject);
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, m_IndexBuffer->GetCount(), GL_UNSIGNED_INT, nullptr);
 			glBindFramebuffer(GL_FRAMEBUFFER, 0);
-			m_Shader->SetUniform4f("u_Color", m_Color.x, m_Color.y, m_Color.z, m_Color.w);
+			//m_Shader->SetUniform4f("u_Color", m_Color.x, m_Color.y, m_Color.z, m_Color.w); // set color of triangle 
 
 			for (Layers::Layer* layer : m_LayerStack)
 			{
--- Blu/engine/src/Blu/Platform/OpenGL/OpenGLBuffer.h
+++ Blu/engine/src/Blu/Platform/OpenGL/OpenGLBuffer.h
@@ -9,10 +9,14 @@ namespace Blu
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void UnBind() const;
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
+
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+		virtual const BufferLayout& GetLayout()const override { return m_Layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 
 	};
 
@@ -22,8 +26,8 @@ namespace Blu
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void UnBind() const;
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
 		virtual uint32_t GetCount() const { return m_Count;}
 
 	private:
