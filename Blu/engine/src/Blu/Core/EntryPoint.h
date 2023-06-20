0d9068818742296cbdff6eb4e77d5e5bd9cb56f3
William <50932749+BilliamsFluster@users.noreply.github.com>
2023-06-19 23:29:04 -0400

    Profiling added

--- Blu/engine/src/Blu/Core/EntryPoint.h
+++ Blu/engine/src/Blu/Core/EntryPoint.h
@@ -6,14 +6,19 @@ extern  Blu::Application* Blu::CreateApplication();
 int main(int argc, char** argv)
 {
 	Blu::Log::Init();
-	BLU_CORE_ERROR("Warning");
-	int a = 5;
-	BLU_TRACE("ClientWarning", a);
+	
 
-
+	BLU_PROFILE_BEGIN_SESSION("Startup", "BluProfile-Startup.json");
 	auto app = Blu::CreateApplication();
+	BLU_PROFILE_END_SESSION();
+
+	BLU_PROFILE_BEGIN_SESSION("Runtime", "BluProfile-Runtime.json");
 	app->Run();
+	BLU_PROFILE_END_SESSION();
+
+	BLU_PROFILE_BEGIN_SESSION("Shutdown", "BluProfile-Shutdown.json");
 	delete app;
+	BLU_PROFILE_END_SESSION();
 }
 #endif // BLU_PLATFORM_WINDOWS
 
