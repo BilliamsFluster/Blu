17873173ddee50e77ede054265f55abee4ffd4f5
William <50932749+BilliamsFluster@users.noreply.github.com>
2023-07-06 17:17:16 -0400

    Multiple Render Targets and Frame Buffer refactored

--- Blu/engine/src/Blu/Platform/OpenGL/OpenGLFrameBuffer.h
+++ Blu/engine/src/Blu/Platform/OpenGL/OpenGLFrameBuffer.h
@@ -9,7 +9,7 @@ namespace Blu
 	public:
 		OpenGLFrameBuffer(const FrameBufferSpecifications& spec);
 		virtual const FrameBufferSpecifications& GetSpecification() const override { return m_Specification; }
-		virtual uint32_t GetColorAttachment() const override { return m_ColorAttachment; }
+		virtual uint32_t GetColorAttachmentID(uint32_t index = 0) const override { BLU_CORE_ASSERT(index < m_ColorAttachments.size()); return m_ColorAttachments[index]; }
 		virtual void Resize(uint32_t width, uint32_t height) override;
 
 		virtual ~OpenGLFrameBuffer() override;
@@ -21,7 +21,14 @@ namespace Blu
 		void Invalidate();
 	private:
 		uint32_t m_RendererID;
-		uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0;
+		//uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0;
 		FrameBufferSpecifications m_Specification;
+
+		std::vector<FrameBufferTextureSpecifications> m_ColorAttachmentSpecs;
+		FrameBufferTextureSpecifications m_DepthAttachmentSpecs;
+		
+		uint32_t m_DepthAttachment;
+		std::vector<uint32_t> m_ColorAttachments;
+		std::vector<uint32_t> m_DepthAttachments;
 	};
 }
